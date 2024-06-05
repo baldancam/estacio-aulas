9794e07d8bf596aec8bf8e730adc167187f6a669
baldancam <baldancama@gmail.com>
2024-06-05 08:07:04 -0300

    add php

---
+++ aula-04-06-2024/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>teste PHP</title>
+</head>
+<body>
+	
+
+<?php
+	// String
+	$nome = 'Matheus teste'
+?>
+
+
+<h1>Teste cadastro:</h1>
+<br />
+<h2><?= $nome ?></h2>
+
+</body>
+</html>
